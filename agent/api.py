e3f7a7351c0914277dc79be492cdfd232335c9cc
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2025-05-13 20:41:25 -0600

    Updated!

--- agent/api.py
+++ agent/api.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+@app.route('/test_args', methods=['GET'])
+def test_args():
+    if request.method =='GET':
+        text = request.get_json()
+        return text
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
