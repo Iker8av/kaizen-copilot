cffde6547304f72d8b98a60cbee2d92035a3f29d
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2025-05-18 18:43:20 -0600

    [MCPHost] Added Context as arg

--- agent/models/mcphost.py
+++ agent/models/mcphost.py
@@ -1,4 +1,8 @@
+from typing import Dict, List
+from agent.agents.embedding_model import EmbeddingModel
 from agent.agents.query_formatter import QueryFormatter
+from agent.models.agentbase import AgentBase
+from agent.models.context import Context
 from agent.models.input import InputBase
 from agent.models.issue import Issue
 from agent.models.mcplog import MCPLog
@@ -9,31 +13,28 @@ from agent.models.output import FileBaseOutput
 class MCPHost:
     def __init__(self):
         self.log = MCPLog()
-        # self.agents: dict[str, str] = {
-        #     # Define Agents
-        # }
-        # self.tools = dict[str, str] = {
-            
-        # }
-        
-        self.workflows = {
-            "issue_resolution": [QueryFormatter()],
+        self.workflows: Dict[str, List[AgentBase]] = {
+            "issue_resolution": [QueryFormatter(), EmbeddingModel()],
         }
 
     def run_workflow(self, name: str, initial_input: InputBase):
         agents = self.workflows.get(name, [])
         input_data = initial_input
+        context = Context(name)
+        
         for agent in agents:
-            output_data = agent.run(input_data)
+            output_data, context = agent.run(input_data, context)
             step = MCPStep(
                 agent_name=agent.__class__.__name__,
+                tool=agent.tool,
                 role=agent.role,
                 purpose=agent.purpose,
+                context=context,
                 input_data=input_data,
                 output_data=output_data,
             )
             self.log.add_step(step)
-            input_data = output_data  # se convierte en input del siguiente paso
-
+            input_data = output_data  
+            
     def get_log(self) -> MCPLog:
         return self.log
