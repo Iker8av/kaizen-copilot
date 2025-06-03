c65ce7bd42351244462e7e5fcf050692b9a69f31
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2025-06-03 00:07:53 -0600

    api

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+from dataclasses import asdict
+import json
 from flask import Flask, request, jsonify
 from agent.models.mcphost import MCPHost
 from agent.models.issue import Issue
@@ -7,21 +9,18 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
     
-@app.route('/fix_issue', methods=['GET'])
+@app.route('/fix_issue', methods=['POST'])
 def fix_issue():
-    if request.method =='GET':
-        issue = Issue()
-        issue.title = request.args.get('title')
-        issue.description = request.args.get('description')
-        issue.repo_url = request.args.get('repo_url')
+    if request.method =='POST':
+        request_data = request.get_json()
+        issue = Issue(title=request_data['title'], description=request_data['description'], repo_url=request_data['repo_url'])
 
         input_data = QueryFormatterInput(issue)
         host = MCPHost()
        
         solution = host.run_workflow('issue_resolution', input_data)
-
-        return jsonify(solution, status=200)
-
+        json_output = json.dumps(asdict(solution), indent=2)
+        return json_output
 
 if __name__ == '__main__':
     app.run()
