d45b72b82ff8405d19044ad38b9d846a4e70cd7e
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2025-06-02 22:51:56 -0600

    Modularity on File Resolver

--- agent/agents/file_resolver.py
+++ agent/agents/file_resolver.py
@@ -17,9 +17,13 @@ class FileResolver(AgentBase[FileInput, Union[FileBaseOutput, QueryFormatterOutp
         self.role: ROLE = ROLE.FILE_RESOLVER
         self.tool: Tool = ChromaDB()
         self.purpose: str = "Ensure LLM Model will have enough context, if not, it wil iterate with the Vector DB"
+        self.__enable_context = True
     
     def run(self, _, context: Context) -> Tuple[Union[FileBaseOutput, QueryFormatterOutput], Context, int]:
         self.context = context
+        
+        if not self.__enable_context:
+            return FileBaseOutput(self.context.retrieved_files), self.context, 1
         
         if len(context.retrieved_files) > 1:
             return FileBaseOutput(self.context.retrieved_files), self.context, 1
@@ -31,8 +35,11 @@ class FileResolver(AgentBase[FileInput, Union[FileBaseOutput, QueryFormatterOutp
             queries: list[str] = []
             conditionals: list[Where] = []
             if extension == "py":
-                from_imports: list[str] = re.findall(r'^\s*from\s+[a-zA-Z_][\w\.]*\s+import\s+[a-zA-Z_][\w]*', code, re.MULTILINE)
+                from_imports: list[str] = re.findall(r'^\s*from\s+[a-zA-Z_.][\w\.]*\s+import\s+[a-zA-Z_][\w]*', code, re.MULTILINE)
                 unique_paths = []
+                
+                possible_names = self.context.issue.repo_name.split("/")
+                from_imports = [s for s in from_imports if any(sub in s for sub in possible_names)]
                 
                 if len(from_imports) <= 0:
                     return FileBaseOutput(self.context.retrieved_files), self.context, 1
@@ -41,6 +48,12 @@ class FileResolver(AgentBase[FileInput, Union[FileBaseOutput, QueryFormatterOutp
                     tokens = include.lstrip().split(" ")
                     dependencies_name = tokens[1]
                     relative_path = dependencies_name.replace(".","/")
+                    
+                    if relative_path[0] == "/":
+                        new_path = context.retrieved_files[0].path.split("/")
+                        new_path[-1] = relative_path
+                        relative_path = "".join(new_path)
+                    
                     imports_names = tokens[-1].split(",")
                     class_names = []
                     function_names = []
@@ -67,7 +80,6 @@ class FileResolver(AgentBase[FileInput, Union[FileBaseOutput, QueryFormatterOutp
                         queries[index] = new_query.strip()
                         continue
                     
-                    print(query)
                     unique_paths.append(relative_path)
                     
                     folders = relative_path.split("/")
